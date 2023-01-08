88573fde8afd9e07718a00272180e477f5b9ca09
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-01-08 15:44:34 +0530

    Frontend completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Setting from "./pages/settings/Setting";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Post from "./components/post/Post";
+import Posts from "./components/posts/Posts";
 
 
 function App() {
   return (
-    <div>
+    <Router>
     <TopBar/>
-    <Single/>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/register">
+         <Register />
+      </Route>
+      <Route path="/login"><Login /></Route>
+      <Route path="/post/:id">
+        <Single />
+      </Route>
+      <Route path="/write"> <Write /></Route>
+      <Route path="/settings">
+        <Setting /> 
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 
--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -2,6 +2,56 @@ import "./singlePost.css"
 
 export default function SinglePost() {
   return (
-    <div>SinglePost</div>
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img src="" alt="" className="singlePostImg" />
+        <h1 className="singlePostTitle">
+        Lorem ipsum dolor
+        <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span>
+            Author:
+            <b className="singlePostAuthor">
+                Safak
+            </b>
+          </span>
+          <span>1 day ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos!
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+    </div>
   )
 }
