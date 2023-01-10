8c8f84e033c561bc538ecd10e92701621ba9448d
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-01-10 13:16:15 +0530

    rest api done

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -20,22 +20,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
-
-//Login
-
+//LOGIN
 router.post("/login", async (req, res) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-      !user && res.status(400).json("Wrong credentials!");
-  
-      const validated = await bcrypt.compare(req.body.password, user.password);
-      !validated && res.status(400).json("Wrong credentials!");
-  
-      const { password, ...others } = user._doc;
-      res.status(200).json(others);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
 
-module.exports = router
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
